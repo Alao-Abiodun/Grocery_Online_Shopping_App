295678117d6c7282a3233e26a3e2243d97fee569
Alao-Abiodun <abiodundev@gmail.com>
2023-01-09 08:56:57 +0100

    chore: add morgan packages

--- src/express-app.js
+++ src/express-app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors  = require('cors');
 const { customer, products, shopping } = require('./api');
-const HandleErrors = require('./utils/error-handler')
+const HandleErrors = require('./utils/error-handler');
+const morgan = require('morgan');
 
 
 module.exports = async (app) => {
@@ -9,6 +10,7 @@ module.exports = async (app) => {
     app.use(express.json({ limit: '1mb'}));
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
     app.use(cors());
+    app.use(morgan('dev'));
     app.use(express.static(__dirname + '/public'))
 
     //api
